3b449fe1ab162c1c88828f6fd0cd46d500b9eb8c
mrteacmbk <tra.phan@smartlook.com.vn>
2023-05-08 14:12:47 +0700

    Update

---
+++ ServerWater2/Models/DataContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ServerWater2.Models
+{
+    public class DataContext : DbContext
+    {
+        public static Random random = new Random();
+       
+        public static string randomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            return new string(Enumerable.Repeat(chars, length).Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+        //public static string configSql = "Host=office.stvg.vn:59066;Database=db_stvg_GIS;Username=postgres;Password=stvg";
+
+        public static string configSql = "";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseNpgsql(configSql);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            //modelBuilder.Entity<SqlArea>().HasMany<SqlUser>(s => s.users).WithMany(s => s.areas);
+            //modelBuilder.Entity<SqlDevice>().HasOne<SqlArea>(s => s.area).WithMany(s => s.devices);
+        }
+    }
+
+}
--- ServerWater2/Program.cs
+++ ServerWater2/Program.cs
@@ -1,35 +1,93 @@
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
+using Serilog.Sinks.SystemConsole.Themes;
+using Serilog;
+using ServerWater2.Models;
+
 namespace ServerWater2
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            Log.Logger = new LoggerConfiguration()
+                   .MinimumLevel.Information()
+                   .WriteTo.Console(theme: AnsiConsoleTheme.Code)
+                   .WriteTo.File("mylog.txt", rollingInterval: RollingInterval.Day)
+                   //.WriteTo.Seq("http://log.smartlook.com.vn:8090", apiKey: "FTidLKHRnxm8y7BaUNvX")
+                   .CreateLogger();
+            try
             {
+                var builder = WebApplication.CreateBuilder(args);
+                builder.WebHost.ConfigureKestrel((context, option) =>
+                {
+                    option.ListenAnyIP(50000, listenOptions =>
+                    {
+
+                    });
+                    option.Limits.MaxConcurrentConnections = 1000;
+                    option.Limits.MaxRequestBodySize = null;
+                    option.Limits.MaxRequestBufferSize = null;
+                });
+                // Add services to the container.
+                builder.Services.AddCors(options =>
+                {
+                    options.AddPolicy("HTTPSystem", builder =>
+                    {
+                        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().SetIsOriginAllowed(origin => true).WithExposedHeaders("Grpc-Status", "Grpc-Encoding", "Grpc-Accept-Encoding");
+                    });
+                });
+                using (StreamReader sr = new StreamReader("Config.txt"))
+                {
+                    string? line = sr.ReadLine();
+                    if (!string.IsNullOrEmpty(line))
+                    {
+                        DataContext.configSql = line;
+                    }
+                }
+                Log.Information("Connected to Server : " + DataContext.configSql);
+
+                builder.Services.AddDbContext<DataContext>(options => options.UseNpgsql(DataContext.configSql));
+                //builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+                builder.Services.AddControllers();
+                // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                builder.Services.AddEndpointsApiExplorer();
+                builder.Services.AddSwaggerGen();
+
+                var app = builder.Build();
+
+                // Configure the HTTP request pipeline.
                 app.UseSwagger();
                 app.UseSwaggerUI();
+
+                app.UseDeveloperExceptionPage();
+                //app.UseMigrationsEndPoint();
+
+                using (var scope = app.Services.CreateScope())
+                {
+                    IServiceProvider services = scope.ServiceProvider;
+                    DataContext datacontext = services.GetRequiredService<DataContext>();
+                    datacontext.Database.EnsureCreated();
+                    await datacontext.Database.MigrateAsync();
+                }
+
+                app.UseCors("HTTPSystem");
+                app.UseRouting();
+
+                app.UseAuthorization();
+
+
+                app.MapControllers();
+                app.MapGet("/", () => string.Format("GIS of STVG - {0}", DateTime.Now));
+               
+                app.Run();
+
             }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
+            catch (Exception ex)
+            {
+                Log.Error(ex.ToString());
+            }
+            Log.CloseAndFlush();
         }
     }
 }
